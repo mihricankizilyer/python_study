c50a2de8cee407c23fbaa480d91e0f215f377015
Mihrican Kızılyer <70093355+mihricankizilyer@users.noreply.github.com>
2021-05-06 19:06:31 +0300

    Update 02_score_calculation.py

--- algorithm_study/02_score_calculation.py
+++ algorithm_study/02_score_calculation.py
@@ -16,6 +16,7 @@ def math(mid1, mid2, final, project):
       return "E"
     elif score <=25 and score >0:
       return "F"
+    
 def physic(mid1, mid2, final, project):
   score = mid1*0.1+ mid2*0.1+ final*0.6+ project*0.2
   if 0<mid1<100 and 0<mid2<100 and 0<final<100 and 0<project<100:
@@ -31,7 +32,9 @@ def physic(mid1, mid2, final, project):
       return "E"
     elif score <=25 and score >0:
       return "F"
+    
 lesson1 = math(100,32,55,11) 
 lesson2 = physic(98,76,85,92)
+
 print("Letter grade in Math: {}".format(lesson1))
 print("Letter grade in Physic: {}".format(lesson2))
